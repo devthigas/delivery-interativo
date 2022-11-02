9c1e2033671bf2c82f58865cc3372b4d066f038b
G-maker191 <guuhkakashi@gmail.com>
2022-11-02 20:36:28 -0300

    implementando paginas de itens

---
+++ itens.php
@@ -0,0 +1,16 @@
+<?php require_once("cabecalho.php"); ?>
+
+<nav class="navbar bg-light fixed-top" style="box-shadow: 0px 3px 5px rgba(0, 0, 0, 0.20);">
+    <div class="container-fluid">
+      <div class="navbar-brand">
+        <a href="index.php">
+          <big><i class="bi bi-arrow-left"></i></big>
+        </a>
+        <span style="margin-left: 15px;">PASTEIS</span>
+      </div>
+      <?php require_once("carrinho.php") ?>
+    </div>
+  </nav>
+
+</body>
+</html>
