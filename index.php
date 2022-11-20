151ac66f00abc8a5164e20af70cc37a7e4bc3f2a
Thigas <thi.deusefiel@gmail.com>
2022-11-20 18:58:29 -0300

    tela de login

--- index.php
+++ index.php
@@ -68,7 +68,6 @@
     <i class="bi bi-whatsapp"></i>
     (19)9 74047622</a></span>
 </footer>
-
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
 </body>
 </html>
